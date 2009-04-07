15fa4b3b392271bf30d25798a7b0b460e70e1aff
David Triendl <david@triendl.name>
2009-04-07 14:29:19 +0200

    Add some read-only tests

--- run_tests.rb
+++ run_tests.rb
@@ -7,8 +7,10 @@ if ARGV[0]
 else
   TahoeServer = 'http://testgrid.allmydata.org:3567/'
 end
+TahoeReadOnlyCap = "URI:DIR2-RO:as2uresriaeed44mvdstfu46je:uhtfyxhbdwbp4zpeda5hydccwt4szrx6dxl27xkmswxo7xmbus4a"
 
 puts "Web API URI: #{TahoeServer}"
+puts "Read onyl cap: #{TahoeReadOnlyCap}"
 
 Dir.foreach "tests" do |file|
   next if file[0] == ?.
